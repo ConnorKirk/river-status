23ba19c906164d65958127dba1042425049c77e7
Connor Kirkpatrick <connor.kirkpatrick@hotmail.com>
2023-01-18 08:04:23 +0000

    Improve cards

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,13 +1,15 @@
 import { useLoaderData } from "@remix-run/react";
 import { getFlowRate, getTideTime } from "~/loaders";
 import indexStyles from "~/styles/index.css";
+import { toPrettyDatetime, toPrettyTime, diffHours } from "~/utils";
 import { Card } from "~/components";
+import { json } from "@remix-run/node";
 
 export const loader = async () => {
   const { flow, dateTime } = await getFlowRate();
   const { events } = await getTideTime();
 
-  return { flow, dateTime, events };
+  return json({ flow, dateTime, events });
 };
 
 export const links = () => {
@@ -17,26 +19,29 @@ export const links = () => {
 export default function Index() {
   const { flow, dateTime, events } = useLoaderData();
   return (
-    <Container>
+    <Wrapper>
       <h1>What's the river doing?</h1>
-      <Flow flow={flow} dateTime={dateTime} />
-      <Board flow={flow} />
-      <Tides events={events} flow={flow} />
-      {/* <Rules /> */}
-    </Container>
+      <Container>
+        <Flow flow={flow} dateTime={dateTime} />
+        <Board flow={flow} />
+        <Tides events={events} flow={flow} />
+        {/* <Rules /> */}
+      </Container>
+    </Wrapper>
   );
 }
 
 const Flow = ({ flow, dateTime }) => (
-  <Card>
-    <p>The stream is</p>
-    <div className="flow-measure">
-      <h2>{flow}</h2>
+  <Card title="Stream">
+    <h2>
+      {flow}
       <i>
+        {" "}
         m<sup>3</sup>/s
       </i>
-    </div>
-    at {dateTime}
+    </h2>
+
+    <span> at {toPrettyDatetime(new Date(dateTime))}</span>
   </Card>
 );
 
@@ -50,30 +55,33 @@ const Board = ({ flow }) => {
   };
 
   return (
-    <Card>
+    <Card title={"Boards"}>
       <p>{getBoard(flow)}</p>
     </Card>
   );
 };
 
 const Tides = ({ events, flow }) => {
-  const nextHighTide = events.filter(
-    ({ eventType, dateTime }) =>
-      eventType === "HighWater" && new Date(dateTime) > new Date()
-  )[0];
+  const nextHighTide = new Date(
+    events.filter(
+      ({ eventType, dateTime }) =>
+        eventType === "HighWater" && new Date(dateTime) > new Date()
+    )[0].dateTime
+  );
 
-  const nextLowTide = events.filter(
-    ({ eventType, dateTime }) =>
-      eventType === "LowWater" && new Date(dateTime) > new Date()
-  )[0];
+  const twoHoursBefore = diffHours(nextHighTide, -2);
+  console.log(twoHoursBefore);
+  const twoHoursAfter = diffHours(nextHighTide, 2);
 
+  console.log({ nextHighTide, twoHoursBefore, t: typeof twoHoursBefore });
   return (
-    <Card>
+    <Card title="Tides">
       {nextHighTide && (
         <>
+          <p>Hightide is at {toPrettyTime(nextHighTide)}.</p>
           <p>
-            Hightide is at {nextHighTide.dateTime}. Richmond Lock will open two
-            hours before and close two hours after.
+            Richmond Lock will open at {toPrettyTime(twoHoursBefore)} and close
+            at {toPrettyTime(twoHoursAfter)}
           </p>
           <p>
             The stream might be {flow - 100} between the lock opening and high
@@ -89,4 +97,34 @@ const Tides = ({ events, flow }) => {
   );
 };
 
+const Rules = () => (
+  <Card>
+    <ol>
+      <li>
+        <h2>Red Boards</h2>
+        <p>No rowing</p>
+      </li>
+      <li>
+        <h2>Orange Boards+</h2>
+        <p>Coxed Fours, Quads and Eights</p>
+        <p>Level 2 Cox with a Coach</p>
+        <p>Turn at Glovers Island</p>
+      </li>
+      <li>
+        <h2>Orange Boards</h2>
+        <p>Fours, Quads and Eights</p>
+        <p>Level 3 Cox, or Level 2 Cox with a Coach</p>
+        <p>Turn at Glovers Island</p>
+      </li>
+      <li>
+        <h2>Yellow Boards</h2>
+        <p>Four blade rule, plus singles with captains permission</p>
+        <p>Level 2 Sign off, or Level 1 with Coach</p>
+        <p>Turn at Glovers Island</p>
+      </li>
+    </ol>
+  </Card>
+);
+
 const Container = ({ children }) => <div className="container">{children}</div>;
+const Wrapper = ({ children }) => <div className="wrapper">{children}</div>;
