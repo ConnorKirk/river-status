22ed3f1cba0c24fed744e7cf89c0626f065632f8
Connor Kirkpatrick <connor.kirkpatrick@hotmail.com>
2023-01-18 08:02:11 +0000

    Add card styling

--- app/components/card/card.js
+++ app/components/card/card.js
@@ -1,3 +1,6 @@
-import "./card.css";
-
-export const Card = ({ children }) => <div className="card">{children}</div>;
+export const Card = ({ title, children }) => (
+  <div className="card">
+    <h1>{title}</h1>
+    {children}
+  </div>
+);
