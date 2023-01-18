06fe28ebe982c91f740c3d2dfd39208d59a16639
Connor Kirkpatrick <connor.kirkpatrick@hotmail.com>
2023-01-18 08:02:57 +0000

    Add formatting funcs

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,3 +1,16 @@
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+
+export const toPrettyDatetime = (dt) => {
+  return new Date(dt).toLocaleTimeString("gb", options);
+};
+
+export const toPrettyTime = (dt) => new Date(dt).toLocaleTimeString("gb");
+
 export const diffHours = (dt, diff) => {
   const copy = new Date(dt);
   copy.setHours(copy.getHours() + diff);
