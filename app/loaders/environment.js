be41fafbd0a06e81bbfd4dcd9b38e45081f580ac
Connor Kirkpatrick <connor.kirkpatrick@hotmail.com>
2024-03-10 16:33:37 +0000

    Break if theres no stream value

--- app/loaders/environment.js
+++ app/loaders/environment.js
@@ -3,10 +3,17 @@ const getFlowRate = () => {
     "https://environment.data.gov.uk/flood-monitoring/id/measures/3400TH-flow--i-15_min-m3_s"
   )
     .then((resp) => resp.json())
-    .then((body) => ({
-      flow: body.items?.latestReading?.value,
-      dateTime: new Date(body.items.latestReading.dateTime),
-    }))
+    .then((body) => {
+      const { latestReading } = body.items;
+
+      if (!latestReading.value) {
+        throw Error("No value from Environment Agency API");
+      }
+      return {
+        flow: body.items.latestReading.value,
+        dateTime: new Date(body.items.latestReading.dateTime),
+      };
+    })
     .catch((err) => {
       console.error(err);
       throw Error(`Error in environment api loader: ${err}`);
